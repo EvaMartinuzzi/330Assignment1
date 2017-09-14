22f3d56135d90ef3faf9ddb6573bfd9d44e4efa6
Eva Martinuzzi <eva.martinuzzi@sportngin.com>
2017-09-14 00:30:59 -0500

    Add search option paths (just interface)

--- NoteTaking/src/Main.java
+++ NoteTaking/src/Main.java
@@ -12,5 +12,58 @@ public class Main {
         scanner.close();
         note.readFiles(path);
         note.getNotesFromFiles();
+
+        System.out.println("Please select how you want to view your notes: ");
+        System.out.println(  "A. Display all notes with mentions (ex: @, #)" +
+                           "\nB. Display all notes, organized by mention (ex @, #" +
+                           "\nC. Display all searched keywords" +
+                           "\nD. Search notes for a keyword as plaintext and mention" +
+                           "\nE. Search all notes for keyword" +
+                           "\nF. Search all notes for keyword as a mention" +
+                           "\nG. Dispaly all notes by occurence of search word");
+        Scanner scanner = new Scanner(System.in);
+        scanner.toLowerCase();
+        String sortChoice = scanner.nextLine();
+        scanner.close();
+
+        if (sortChoice=="a"){
+            //show all notes with mentions
+        }
+        if (sortChoice=="b"){
+            //show all notes organized by mention
+        }
+        if (sortChoice=="c"){
+            //show all searched keywords
+        }
+
+        switch (sortChoice){
+            case 2:  monthString = "d";
+                search(monthString);
+                break;
+            case 3:  monthString = "e";
+                search(monthString);
+                break;
+            case 4:  monthString = "f";
+                search(monthString);
+                break;
+            case 5:  monthString = "g";
+                search(monthString);
+                break;
+        }
+
+        String search(String x){
+            System.out.println("Please enter the keyword you would like to search: ");
+            Scanner scanner = new Scanner(System.in);
+            String keyword = scanner.nextLine();
+            if (x=="e"){
+                //search just as keyword
+            }
+            else if (x=="f"){
+                //search as a mention
+            }
+            else if (x=="g"){
+                //sort by occurence of search word
+            }
+        }
     }
 }
